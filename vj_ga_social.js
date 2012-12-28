a5eb9fb7912f6b869e96ac5ccc919324272651fd
tf <tf@tonyfelice.net>
2012-12-28 19:11:44 +0000

    cleanup and comments

--- vj_ga_social.js
+++ vj_ga_social.js
@@ -4,7 +4,7 @@ USAGE
 <script src="vj_ga_social.js"></script>
 <script>
     testSocial({
-        facebook: 'facebook_app_id'
+        facebook: 'exampleAppId1234567890'
     });
 </script>
  
@@ -13,7 +13,7 @@ USAGE
  */
 (function (window, undefined) {
 	var beacon
-	,loadScr = function(src){
+	,loadScr = function(src){  //handler for loading scripts
 	    var doc = window.document
 	    ,tag = 'script'
 	    ,g=doc.createElement(tag)
@@ -21,20 +21,25 @@ USAGE
 	    g.src=src;
 	    s.parentNode.insertBefore(g,s);
 	}
-	,loadImg = function(src, name){
+	,loadImg = function(src, name){ //handler for loading images
 	    var i = new Image();
 	    i.onload=function(){logStatus(name, true); i = i.onload = i.onerror = undefined; };
 	    i.onerror=function(){logStatus(name, false); i = i.onload = i.onerror = undefined; };
 	    i.src=src;
 	}
-	,logStatus = function(network, status) {
-	    if(status){
-		//window._gaq.push(['_trackEvent', 'social', network, 'logged in',  undefined, 1]);
+	,logStatus = function(network, status) { //handler for status
+	    if(status){  //only tracking positives
+		window._gaq.push(['_trackEvent', 'social login status', network, 'logged in',  undefined, 1]);
 	    }
-	    window.logData(network +' '+ status);
 	}
-	,beacons = {
-		facebook: {
+	,beacons = { //this is the json that controls which networks are supported.  beacon (object) whose only child is src trigger handiling via image. beacon with src and init children are handled as script
+		/*
+		 * facebook has a supported API method to check login status without interaction.
+		 * Pass the beaconName ("facebook") as an array key when calling the testSocial function.
+		 * The value of the array key should be the appId from fb.
+		 * 
+		 */
+		facebook: { 
 		    src: '//connect.facebook.net/en_US/all.js',
 		    init: function(name){
 			window.fbAsyncInit = function(){
@@ -45,6 +50,9 @@ USAGE
 			};
 		    }
 		}
+		/*
+		 * the rest of these networks don't have supported methods, so we're requesting images that are restricted to logged in users, and triggering onload / onerror accordingly
+		 */
 		,twitter: {
 		    src: 'https://twitter.com/login?redirect_after_login=%2Fimages%2Fspinner.gif'
 		}
@@ -58,7 +66,7 @@ USAGE
 		    src: 'https://pinterest.com/login/?next=https://s-passets-ec.pinimg.com/images/load2.gif'
 		}
 	}
-	,init = function () {
+	,init = function () {  //loop the beacons json and handle accordingly.
 	    for (var name in beacons) {
 		beacon = beacons[name];
 		if (beacon.appId) {
@@ -69,12 +77,11 @@ USAGE
 		}
 	    }
 	}
-	,testSocial = function (opts) {
-	    window._gaq = window._gaq || [];
+	,testSocial = function (opts) {  //init the function using the object passed to function, which comes through as opts
 	    if (opts.facebook) {
 		beacons.facebook.appId = opts.facebook;
 	    }
-	    if (opts.callback) {
+	    if (opts.callback) {  //override native logStatus handler with custom callback
 		logStatus = opts.callback;
 	    }
 	    init();
